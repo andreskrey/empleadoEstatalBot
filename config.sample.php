c64bfe2af4f861e40328545a8c7522388efdb1b7
Andres Rey <andreskrey@gmail.com>
2016-08-17 13:08:37 +0100

    Config sample

--- config.sample.php
+++ config.sample.php
@@ -2,6 +2,14 @@
 
 class empleadoEstatalConfig
 {
-    static $CLIENT_ID = '';
-    static $SECRET_KEY = '';
+    static $CLIENT_ID = 'CLIENT_ID';
+    static $SECRET_KEY = 'SECRET_KEY';
+    static $REDIRECT_URI = 'REDIRECT_URI';
+
+    static $SCOPES = ['submit', 'read'];
+
+    static $USERNAME = 'USERNAME';
+    static $PASSWORD = 'PASSWORD';
+
+    static $SIGNATURE = 'SIGNATURE EN HTML';
 }
