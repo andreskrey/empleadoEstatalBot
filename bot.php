2119d26a3cc5af609a55f4ac3f802b8f64bf9cf6
Andres Rey <andreskrey@gmail.com>
2016-08-17 14:00:55 +0100

    Rollback for a bit

--- bot.php
+++ bot.php
@@ -10,7 +10,7 @@ use League\HTMLToMarkdown\HtmlConverter;
 class empleadoEstatal
 {
     private $subreddits = [
-        'argentina'
+        'empleadoEstatalBot'
     ];
 
     private $newspapers = [
