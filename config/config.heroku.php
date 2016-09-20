528fa499b7fb663dbbabbd7a46d6da2e065a4e2a
Andres Rey <andreskrey@gmail.com>
2016-09-20 20:58:27 +0100

    DAFUCK

--- config/config.heroku.php
+++ config/config.heroku.php
@@ -16,7 +16,7 @@ class Config
     static $REDIS_URL = null;
 
     static $SIGNATURE = '<hr><p><em><strong>empleadoEstatalBot</strong>, el primer bot de reddit 100% peronista. Ensamblado en TdF. ^(Hecho en China.)</em></p><p><a href="/u/subtepass">Autor</a>
-    | <a href="https://github.com/andreskrey/empleadoEstatalBot">Código fuente</a> | <a href="https://github.com/andreskrey/empleadoEstatalBot#que-diarios-soporta">Lista de diarios <soportados></soportados></a></p>';
+    | <a href="https://github.com/andreskrey/empleadoEstatalBot">Código fuente</a> | <a href="https://github.com/andreskrey/empleadoEstatalBot#que-diarios-soporta">Lista de diarios soportados</a></p>';
 
     public function __construct()
     {
