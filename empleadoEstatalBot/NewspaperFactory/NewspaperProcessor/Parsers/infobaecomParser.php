c12e13221731f577d800e36f54e91cfd62f24169
Andres Rey <andreskrey@gmail.com>
2016-10-05 15:36:24 +0100

    Fix infobaeParser... again

--- empleadoEstatalBot/NewspaperFactory/NewspaperProcessor/Parsers/infobaecomParser.php
+++ empleadoEstatalBot/NewspaperFactory/NewspaperProcessor/Parsers/infobaecomParser.php
@@ -20,11 +20,15 @@ class infobaecomParser extends NewspaperProcessor
         $html = '<!DOCTYPE html><html><head><title></title></head><body>';
         // Hay que pasar por xpath en vez de agarrar el h1 por que infobae a veces usa el h1 para noticias en el header,
         // antes del titulo de la noticia que esta parseando
-        $html .= '<h1>' . $xpath->query("//*[contains(@class, 'entry-title')]")->item(0)->nodeValue . '</h1>';
-        $html .= '<h2>' . $xpath->query("//*[contains(@class, 'preview')]")->item(0)->nodeValue . '</h2>';
+        $html .= '<h1>' . $this->dom->getElementsByTagName('header')->item(0)->getElementsByTagName('h1')->item(0)->nodeValue . '</h1>';
+        $html .= '<h2>' . $xpath->query("//*[contains(@class, 'subheadline')]")->item(0)->nodeValue . '</h2>';
 
-        $html .= $this->dom->saveHTML($xpath->query("//*[contains(@class, 'cuerposmart')]")->item(0)->getElementsByTagName('div')->item(0));
-
+        foreach ($this->dom->getElementById('article-content')->childNodes as $i) {
+            if (trim($i->nodeValue)) {
+                if (mb_strpos(mb_strtolower(trim($i->nodeValue)), 'lea más') === 0) break;
+                $html .= $this->dom->saveHTML($i);
+            }
+        }
         $html .= Config::$SIGNATURE;
         $html .= '</body></html>';
 
